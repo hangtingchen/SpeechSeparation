59d14da53518c09e7a25c6b6680f551523e7990f
hangtingchen <chenhangting@hccl.ioa.ac.cn>
2020-08-06 23:41:44 +0800

    Update path.sh

--- path.sh
+++ path.sh
@@ -4,6 +4,5 @@ export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
-#export PATH=/nobackup/s1/asr/zhangyike/Anaconda3/bin/:$PATH
 export PATH=./lns/:$PATH
 
